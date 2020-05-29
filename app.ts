50cde020343208a9b71ff6d4ee9f9c0059a2e476
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-05-29 19:55:34 +0530

    Integrated: the auth router

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
 import {Application} from "https://deno.land/x/denotrain@v0.5.0/mod.ts";
 
 import config from "./utils/config.ts"
+import router from "./routers/auth_router.ts";
 
 const app = new Application({port: config.PORT});
 
-app.use("/", (ctx) => {
+app.use("/api/auth", router);
+
+app.get("/", (ctx) => {
     return {sucess: true, message: "Welcome to DenoLand!"}
 });
+
 
 await app.run();
