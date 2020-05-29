ba884ed42f20b8e9d96174a8e47c769ee664f1f8
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-05-29 21:12:02 +0530

    Added: a centralized deps file

---
+++ deps.ts
@@ -0,0 +1,5 @@
+export {Application, Router} from "https://deno.land/x/denotrain@v0.5.0/mod.ts";
+export {MongoClient} from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+export * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
+export { validateJwt } from "https://deno.land/x/djwt/validate.ts"
+export { makeJwt, Jose, Payload } from "https://deno.land/x/djwt/create.ts"
