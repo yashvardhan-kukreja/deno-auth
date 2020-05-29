3ba305909a7f923cf33deae1ba8b714290039e0e
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-05-29 22:29:48 +0530

    Added: env variables support

--- utils/config.ts
+++ utils/config.ts
@@ -1,8 +1,10 @@
+import "https://deno.land/x/dotenv/mod.ts";
 const config = {
-    "PORT": 8000,
-    "DB_URI": "mongodb://localhost:27017",
-    "DB": "deno-auth",
-    "JWT_KEY": "hello1234322432"
+    "PORT": Deno.env.get("PORT") || 8000,
+    "DB_URI": Deno.env.get("DB_URI") || "mongodb://localhost:27017",
+    "DB": Deno.env.get("DB") || "deno-auth",
+    "JWT_KEY": Deno.env.get("JWT_KEY") || "hello1234322432"
 };
+console.log(config);
 
 export default config;
